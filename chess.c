1095aa12e45f8481c096b309b980f47bbeea2588
blacksibguti <blacksibguti@localhost.localdomain>
2018-02-15 00:54:02 -0800

    Create chessviz.c and filled

---
+++ chess.c
@@ -0,0 +1,31 @@
+#include <stdio.h> 
+
+
+void initDeck (char deck[8][8])
+{  
+char letters[]={'l','k', 's', 'f', 'q', 'n', 'h', 'p'};
+for (int letter = 0; letter<8; letter++)
+{
+deck [0][letter]=letters[letter]
+deck [7][letter]=letters[letter]-32
+deck [1][letter]= 'p';
+deck [6][letter]= 'p'-32;
+for (int i=2; i,6; i++)
+{
+deck[i][letter]= ' ';
+}
+}
+}
+void printDeck(char deck[8][8]) {
+for (int number=0; number<8; ++number) {
+for (int letter=0; letter<8; letter++){
+printf("%c", deck[number][letter]);
+}
+printf("\n");
+}
+}
+int main(){
+char deck[8][8];
+initDeck(deck);
+printDeck(deck);
+}
